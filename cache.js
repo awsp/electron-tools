2c261577566d98ac8dbf59db461e4ba9d8433e5f
awsp <anthonyspwu@gmail.com>
2015-08-06 10:03:56 -1000

    add more controls to be able to close windows, as Windows env does not allow windows to be hidden

--- cache.js
+++ cache.js
@@ -2,61 +2,110 @@
  * Cache.js
  *
  * Cache BrowserWindows so we can reference it anywhere in main process.
- * Provides a convenient to preserve BrowserWindow's state.  
+ * Provides a convenient way to preserve BrowserWindow's state.
  */
-var BrowserWindow = require('browser-window');
+var BrowserWindow = require("browser-window");
+
+var AppWindow = (function () {
+  /**
+   * Class Constructor
+   */
+  function AppWindow(instance, id, triggerQuit) {
+    this.id = id;
+    this.instance = instance;
+    this.triggerQuit = false;
+
+    if (triggerQuit) {
+      this.triggerQuit = triggerQuit;
+    }
+  }
+
+  return AppWindow;
+})();
+
 var cache = {
 
   // Hold all references to BrowserWindows
   list: {},
 
   // Close options, hide|close
-  closeOptions: ['hide', 'close'],
+  closeOptions: ["hide", "close"],
 
   // Default close option
-  defaultCloseOption: 'hide',
+  defaultCloseOption: "hide",
 
   // Indicating if app is really quitting.
   isAppQuitting: false,
 
   // Create a new instance of BrowserWindow, inserted into our list to allow us
   // to have random access to all BrowserWindows.
-  add: function (name, options, url) {
+  add: function (name, options, url, quit) {
     if (cache.has(name)) {
-      console.error('Window already exists. ');
+      console.error("Window already exists. ");
       return false;
     }
 
     var bw = new BrowserWindow(options);
-
-    // Load page if url is provided.
     if (url) {
       bw.loadUrl(url);
     }
 
-    // Prevent default window close option, hide window instead,
-    // so we can preserve window's state even we "closed" the window.
-    if (cache.defaultCloseOption === 'hide') {
-      bw.on('close', function (event) {
-        if (!cache.isAppQuitting) {
-          event.preventDefault();
-          this.hide();
-          return false;
-        }
-        else {
-          // Prevent memory leaks.
-          // We are going to destroy all windows when app is truely quitting.
-          cache.remove(name);
-        }
-      });
+    bw.on("close", function (event) {
+      if (cache.list[name].triggerQuit) {
+        cache.isAppQuitting = true;
+      }
+
+      if (!cache.isAppQuitting) {
+        event.preventDefault();
+        this.hide();
+        return false;
+      }
+      else {
+        cache.remove(name);
+      }
+    });
+    bw.on("closed", function (event) {
+      if (cache.list[name].triggerQuit) {
+        require("app").quit();
+      }
+      else {
+        cache.list[name].status = "closed";
+        cache.remove(name);
+      }
+    });
+    var appWindow = new AppWindow(bw, bw.id);
+    if (quit) {
+      appWindow.triggerQuit = quit;
     }
 
-    cache.list[name] = bw;
+    cache.list[name] = appWindow;
   },
 
-  get: function (name) {
+  /**
+   * Get a window based on its name
+   *
+   * @param String name
+   * @param Boolean raw - indicate if it should return an instance or not.
+   */
+  get: function (name, raw) {
     if (name && cache.has(name)) {
-      return cache.list[name];
+      if (raw) {
+        return cache.list[name];
+      }
+      return cache.list[name].instance;
+    }
+    return null;
+  },
+
+  find: function (callback, shouldReturn) {
+    var win;
+    for (var key in cache.list) {
+      if (callback(cache.list[key])) {
+        if (shouldReturn && shouldReturn === "key") {
+          return key;
+        }
+        return cache.list[key];
+      }
     }
     return null;
   },
@@ -85,19 +134,40 @@ var cache = {
     }
   },
 
-  // Alias to .get([windowKey]).show()
   show: function (name) {
-    var win = cache.get(name);
+    var win = cache.get(name, true);
     if (win) {
-      win.show();
+      win.instance.show();
     }
   },
 
-  // Alias to .get([windowKey]).hide()
+
   hide: function (name) {
-    var win = cache.get(name);
+    var win = cache.get(name, true);
     if (win) {
-      win.hide();
+      win.instance.hide();
+    }
+  },
+
+  open: function(name) {
+    var appWindow = cache.get(name, true);
+    appWindow.instance.show();
+  },
+
+  close: function (instance) {
+    if (typeof instance === "string") {
+      if (cache.defaultClosingOption === "hide") {
+        cache.hide(instance);
+      }
+      else if (cache.defaultClosingOption === "close") {
+        cache.get(instance).close();
+      }
+    }
+    else if (typeof instance === "object") {
+      var winKey = cache.find(function (win) {
+        return win.id == instance.id;
+      }, "key");
+      cache.close(winKey);
     }
   },
 
